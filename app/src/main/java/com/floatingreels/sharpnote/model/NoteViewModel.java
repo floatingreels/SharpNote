a38e67b727c42705426013eccecf14771d1d7a33
floatingreels <david.gunzburg@gmail.com>
2020-02-24 22:48:48 +0100

    Changed layout of cards in Note list fragment

--- app/src/main/java/com/floatingreels/sharpnote/model/NoteViewModel.java
+++ app/src/main/java/com/floatingreels/sharpnote/model/NoteViewModel.java
@@ -9,6 +9,15 @@ public class NoteViewModel extends ViewModel {
 
     private MutableLiveData<ArrayList<Note>> notes;
 
+    //CREATE
+    private void loadNotes(){
+        ArrayList<Note> allNotes = new ArrayList<>();
+        allNotes.add(new Note("To-do list", "find job\nget rich\ndie trying"));
+        allNotes.add(new Note("Zakdoek Raketkanon","afmetingen 39 x 39 cm, misschien kader kopen in Lucas Creativ?"));
+        allNotes.add(new Note("Deurbel", "nieuwe deurbel gaan kopen in Gamma of Brico"));
+        notes.setValue(allNotes);
+    }
+
     //READ
     public MutableLiveData<ArrayList<Note>> getAllNotes() {
         if (notes == null){
@@ -16,11 +25,5 @@ public class NoteViewModel extends ViewModel {
             loadNotes();
         }
         return notes;
-    }
-
-    private void loadNotes(){
-        ArrayList<Note> allNotes = new ArrayList<>();
-        allNotes.add(new Note("to do list", "find job\nget rich\ndie trying"));
-        notes.setValue(allNotes);
     }
 }
